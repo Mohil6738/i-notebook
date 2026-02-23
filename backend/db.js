c3b189b3ff2ae4ba0e99733263513c32b62e54fa
Mohil Garg <gargmohil19@gmail.com>
2026-02-23 12:32:16 +0530

    adding data validation using express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/INotebook";
 
 const connectToMongo = async () => {
   try {
